4e5ac665d76d6cd160d96d0b5148cf1c2ea0b3f6
TF <tfalase24@gmail.com>
2024-03-08 12:48:27 +0000

    Created Push Split as first Split Spring Bean

---
+++ src/main/java/com/example/demo/Split/PushSplit.java
@@ -0,0 +1,18 @@
+package com.example.demo.Split;
+
+import com.example.demo.Workout.Workout;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PushSplit implements Split{
+
+    @Override
+    public String pickATypeOfSplit() {
+        return "Push Workout";
+    }
+
+    @Override
+    public String toString(){
+        return "Push Split";
+    }
+}
