1596d74f76a34e60448ee31b4f2730ae1a0f04db
TF <tfalase24@gmail.com>
2024-03-08 12:50:05 +0000

    Created Split Controller that uses Spring framework to find Split Spring Bean for Dependency Injection in constructor

---
+++ src/main/java/com/example/demo/Split/SplitController.java
@@ -0,0 +1,22 @@
+package com.example.demo.Split;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SplitController {
+
+    private Split split;
+
+    @Autowired
+    public SplitController(Split split){
+        this.split = split;
+    }
+
+    @GetMapping("/split")
+    public String pickYourSplit(){
+        return "Pick Your Split: " + split.toString();
+    }
+
+}
