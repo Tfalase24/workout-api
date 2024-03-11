3c614adeb068333106a83b0afdd816e7b73f7362
TF <tfalase24@gmail.com>
2024-03-11 16:29:44 +0000

    Created Enum for to classify exercises by different muscle groups

---
+++ src/main/java/com/example/demo/exercise/MuscleGroup.java
@@ -0,0 +1,16 @@
+package com.example.demo.exercise;
+
+public enum MuscleGroup {
+    CHEST,
+    BACK,
+    BICEP,
+    TRICEP,
+    SHOULDER,
+    QUADRICEP,
+    HAMSTRING,
+    GLUTE,
+    CALF,
+    FOREARM,
+    TIBIALIS,
+    NECK
+}
