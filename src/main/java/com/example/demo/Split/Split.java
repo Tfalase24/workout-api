b8ff374e16f5a06bc4a6729d3cf3fc161e41d7f0
TF <tfalase24@gmail.com>
2024-03-11 11:27:23 +0000

    Added Post Construct method to Split interface

--- src/main/java/com/example/demo/Split/Split.java
+++ src/main/java/com/example/demo/Split/Split.java
@@ -1,8 +1,14 @@
 package com.example.demo.Split;
 
 import com.example.demo.Workout.Workout;
+import jakarta.annotation.PostConstruct;
 
 public interface Split {
 
     String pickATypeOfWorkout();
+
+    @PostConstruct
+    default void chooseYourWorkout(){
+        System.out.println("Split has been created");
+    };
 }
