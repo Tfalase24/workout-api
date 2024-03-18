e4b7dc305700806dbfc334e7799c8940f485cdf5
TF <tfalase24@gmail.com>
2024-03-18 09:22:20 +0000

    Added commandLine runner to add exercise to database as the application starts

--- src/main/java/com/example/demo/WorkoutApiApplication.java
+++ src/main/java/com/example/demo/WorkoutApiApplication.java
@@ -1,7 +1,13 @@
 package com.example.demo;
 
+import com.example.demo.exercise.*;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @SpringBootApplication
 public class WorkoutApiApplication {
@@ -10,4 +16,19 @@ public class WorkoutApiApplication {
 		SpringApplication.run(WorkoutApiApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(ExerciseDAO exerciseDAO){
+		return runner -> {
+			System.out.println("Command Line Runner has started");
+			addExerciseToDatabase(exerciseDAO);
+			System.out.println("Command Line Runner has ended");
+		};
+	}
+
+	private void addExerciseToDatabase(ExerciseDAO exerciseDAO){
+		System.out.println("Creating new exercise object");
+		ExerciseClass squat = new ExerciseClass("Squat", DifficultyOfExercise.MEDIUM, new HashSet<>(Set.of(MuscleGroup.GLUTE, MuscleGroup.QUADRICEP)), new HashSet<>(Set.of(MuscleGroup.HAMSTRING, MuscleGroup.CALF)), ExerciseType.SHORTENING);
+		System.out.println("Saving the Exercise");
+		exerciseDAO.save(squat);
+	}
 }
