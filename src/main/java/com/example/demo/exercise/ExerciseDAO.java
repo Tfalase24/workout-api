90c67d3aa2e0a448e0248d4388eef23776ba2c10
TF <tfalase24@gmail.com>
2024-03-27 16:18:22 +0000

    Added future ExerciseDAOImpl methods

--- src/main/java/com/example/demo/exercise/ExerciseDAO.java
+++ src/main/java/com/example/demo/exercise/ExerciseDAO.java
@@ -1,14 +1,26 @@
 package com.example.demo.exercise;
 
+import java.util.List;
+
 public interface ExerciseDAO {
 
     void save(Exercise exercise);
 
-    Exercise findExercise(String name);
+    Exercise findExerciseByName(String name);
 
     Exercise findByMuscleGroup(MuscleGroup muscleGroup);
+
+    List<Exercise> findAllExercisesByMuscleGroup(MuscleGroup muscleGroup);
+
+    List<Exercise> findAllExercisesByAntagonistMuscleGroup(MuscleGroup muscleGroup);
+
+    List<Exercise> findAllExercisesByDifficulty(DifficultyOfExercise difficultyOfExercise);
+
+    List<Exercise> findAllExercisesByExerciseType(ExerciseType exerciseType);
 
     void updateExercise(Exercise exercise);
 
     void deleteExercise(String name);
+
+
 }
